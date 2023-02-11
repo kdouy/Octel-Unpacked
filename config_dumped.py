f538d86d6bdcad73fd2fd1d517214d92fc09388e
jump_ <53615275+arrayee@users.noreply.github.com>
2023-02-11 21:39:34 +0700

    Add files via upload

---
+++ config_dumped.py
@@ -0,0 +1,10 @@
+# This file is unpacked by zlibdeob (Vault) 
+
+__CONFIG__ = {
+    'systeminfo': True,
+    'startup': True,
+    'injection': True,
+    'discordtoken': True,
+    'browsers': True,
+    'antidebug': True,
+    'webhook': "not showing this lol" }
